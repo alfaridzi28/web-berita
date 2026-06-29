d0d92e85be625ef72745fb3360568c428a883c36
Muhammad Yusuf Rifqi Al Faridzi <alfaridzi.rifqi28@gmail.com>
2026-06-30 00:40:43 +0700

    Configure bootstrap cache paths for Vercel

--- api/index.php
+++ api/index.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Prevent Laravel from writing to the read-only bootstrap/cache directory
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/cache/config.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/cache/events.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/cache/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/cache/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/cache/services.php';
+
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
@@ -17,6 +24,7 @@ $app->useStoragePath('/tmp');
 $storagePath = $app->storagePath();
 
 $directories = [
+    '/tmp/cache',
     $storagePath . '/app',
     $storagePath . '/framework/cache/data',
     $storagePath . '/framework/sessions',
